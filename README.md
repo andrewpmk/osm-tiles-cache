81beb0d6e789581671578341ac9fbe195aab053a
andrewpmk <andrewpmk@gmail.com>
2018-05-20 16:03:06 -0400

    Add files via upload
    Update load.php to output pngs downloaded from tile server first before writing to disk, to redownload tiles older than 1 week, and add comments to code to make it easier to understand. Add documentation to README.

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ osm-tiles-cache
 ===============
 
 PHP-based cache for openstreetmap tiles
+Copyright (c) 2018 Andrew MacKinnon
+Copyright (c) 2014 Stefan Lemme
+
+Released under the MIT license, see LICENSE for more information.
+
+Requirements: A Linux server with Apache and PHP installed.
+
+How to install:
+
+1. Create a directory /var/www/html/api/tiles/
+2. Copy the load.php file to /var/www/html/api/tiles/
+3. Copy the provided .htaccess file to /var/www/html/api/tiles/ and enable mod_rewrite support. This will rewrite requests for tiles like /api/tiles/osm/10/283/370.png to load.php.
+4. Create a folder /var/www/html/api/tiles/preload/. For each tile server you want to use, copy a .php.sample to name.php where name is the name of the tile server that you wish to use. Please see https://wiki.openstreetmap.org/wiki/Tile_servers for a list of tile servers. Make sure you have permission to use the tile server you wish to use. Please be advised that the Mapbox tile server service does not permit tile caching under its terms of service.
+5. Optional: There is also a preload.php which pre-downloads tiles and places them into the cache directory. If you wish, you can also copy this file to /var/www/html/api/tiles/ to enable the preloader. WARNING: ALWAYS add an entry to .htaccess that either restricts access to this file to the localhost or requires a password to view preload.php. Otherwise, a malicious user can download a large area of tiles and fill up all your disk space! Also, keep in mind that some tile providers may dislike you using preload.php because it downloads a large number of tiles and uses up a large amount of server resources.
+6. Strongly recommended:
+
+Run sudo crontab -e and add the following line to crontab:
+
+0 7 * * * find /var/www/html/api/tiles/cache/ -mindepth 1 -mtime +7 -delete
+
+This deletes any tile in the tile cache directory every day at 7:00 UTC which is older than 1 week (+7). Change the directory, age and time to run the cron script as appropriate. This will prevent the tile cache directory from taking up too much disk space from old tiles which we never will use anyway.
--- load.php
+++ load.php
@@ -1,10 +1,44 @@
 <?php
+// OpenStreetMap tile cache to save bandwidth of tile servers in order to save money on tile hosting costs
+// by Andrew MacKinnon
+// Adapted from https://github.com/stlemme/osm-tiles-cache by Stefan Lemme
+// Last updated May 20, 2018
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2018 Andrew MacKinnon
+// Copyright (c) 2014 Stefan Lemme
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+$max_cache_time = 60*60*24*7; // maximum time to cache a tile before redownloading it in seconds (default 1 week)
+
+// The following parameters must be set order for the tile cache to work: x, y, z, provider
+// See https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames for how the naming convention of OSM tiles works
+// provider = provider of map tiles. For each provider there is a .php file in the 
+// Tiles are fetched from the /api/tiles/provider/z/x/y.png directory
+// Apache mod_rewrite is used to map this to parameters in load.php file
 
 if (!isset($_GET["x"]) || !isset($_GET["y"]) || !isset($_GET["z"]) || !isset($_GET["provider"])) {
 	header("HTTP/1.1 404 Not Found");
 	die("Missing parameters!");
 }
-
 
 $x = intval($_GET["x"]);
 $y = intval($_GET["y"]);
@@ -12,6 +46,7 @@ $z = intval($_GET["z"]);
 
 $provider = $_GET["provider"];
 
+// Provider configuration file is in provider/$provider.php
 $provider_config = __DIR__ . "/provider/" . $provider . ".php";
 
 if (!file_exists($provider_config)) {
@@ -21,9 +56,11 @@ if (!file_exists($provider_config)) {
 
 require($provider_config);
 
-
+// In each $provider.php there is an array called $urls with urls of 1 or more tile servers to download from
+// We randomly choose one of the tile servers if more than one tile server is specified
 $url = $urls[array_rand($urls)];
 
+// PNGs are placed in the /api/tiles/cache/provider/ directory
 $cache_dir = realpath(__DIR__ . "/cache");
 
 if (!is_dir($cache_dir))
@@ -31,22 +68,33 @@ if (!is_dir($cache_dir))
 
 $path = $cache_dir . "/" . $provider . "/" . $z . "/" . $x. "/";
 
-if (!is_dir($path)) {
-	if (!mkdir($path, 0755, true)) {
-		header("HTTP/1.1 500 Internal Server Error");
-		die("Could not create path: " . $path);
-	}
-}
-
+// The file to load cached tile from and save the cached tile to if not already there
 $file = $path . $y . ".png";
 
-if (!copy($url, $file)) {
-	header("HTTP/1.1 404 Not Found");
-	die("Could not copy remote file: " . $url);
+// If file already exists locally and not older than $max_cache_time
+// Then output the file from disk
+// Otherwise download it
+if (file_exists($file) && time()-filemtime($file) < $max_cache_time) {
+	header("HTTP/1.1 200 Found");
+	header("Content-type: image/png");
+	echo file_get_contents($file);
+} else {
+	$png = file_get_contents($url);
+	if ($png === FALSE) {
+		header("HTTP/1.1 404 Not Found");
+		die("Could not download remote file: " . $url);
+	} else {
+		header("HTTP/1.1 200 Found");
+		header("Content-type: image/png");
+		echo $png;
+		// Create directory for cache if it does not exist already
+		if (!is_dir($path)) {
+			if (!mkdir($path, 0755, true)) {
+				header("HTTP/1.1 500 Internal Server Error");
+				die("Could not create path: " . $path);
+			}
+		}
+		file_put_contents($file,$png);
+	}
 }
-
-header("HTTP/1.1 200 Found");
-header("Content-type: image/png");
-echo file_get_contents($file);
-
 ?>
